a1a17bdfbe3405ec0effefe09d6e329df90293a5
kiuchanlee <kiuchanlee@gmail.com>
2025-04-28 18:42:03 +0900

    Create main_pipeline.py

---
+++ src/main_pipeline.py
@@ -0,0 +1,8 @@
+from hra_news_step0 import main as step0_main
+from hra_news_step1 import main as step1_main
+from hra_news_step2 import main as step2_main
+
+if __name__ == "__main__":
+    step0_main()
+    step1_main()
+    step2_main()
