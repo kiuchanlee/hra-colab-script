9217e839b25c4a589a7df0aac6402df4d0a3c688
kiuchanlee <kiuchanlee@gmail.com>
2025-04-29 14:44:49 +0900

    Update hra_news_step2.py

--- src/hra_news_step2.py
+++ src/hra_news_step2.py
@@ -10,7 +10,7 @@ from datetime import datetime
 
 from utils.logger import log_info, log_error
 from utils.file_manager import get_today_folder, get_today_filename
-from utils.gpt_utils import summarize_all_in_3_lines, analyze_articles_batch
+from utils.gpt_utils import analyze_articles_batch
 from utils.sheet_uploader import upload_to_google_sheet
 
 # ✅ 본문 수집 함수
@@ -52,8 +52,8 @@ def main():
     tqdm.pandas()
     df["본문"] = df["URL"].progress_apply(get_naver_news_body)
 
-    # ✅ 요약 (선택 적용)
-    df = summarize_all_in_3_lines(df)
+    # # ✅ 요약 (선택 적용)
+    # df = summarize_all_in_3_lines(df)
 
     # ✅ 시트 업로드
     sheet_id = "1l89Eca3CsjLEjG-9_raVMy6Y_sYE4BLA-XRtgwEhHEc"  # <- 필요 시 수정
